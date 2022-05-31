9a1ad74bb2df9e7a4633db90fb9bb3da6ceda7fe
NAGIBATYA <itreboganov@gmail.com>
2022-06-01 00:06:49 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,9 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*ActionListener messageAL = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                Date date = new Date();
-                System.out.println("Новое Сообщение! Получено в " + dateFormat.format(date));
-            }
-        };
-        Timer myTimer = new Timer(3000, messageAL);
-        myTimer.start();
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();*/
         Window w = new Window();
         w.run();
     }
--- src/Window.java
+++ src/Window.java
@@ -5,12 +5,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ImageObserver;
+import java.awt.image.RenderedImage;
+import java.awt.image.renderable.RenderableImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.AttributedCharacterIterator;
+import java.util.Map;
 
 public class Window extends JFrame implements ActionListener, KeyListener {
-
 
     private Toolkit tk = Toolkit.getDefaultToolkit();
     private JPanel panel;
@@ -24,6 +33,7 @@ public class Window extends JFrame implements ActionListener, KeyListener {
     private int xssize = (int) tk.getScreenSize().getWidth();            //1600
     private int yssize = (int) tk.getScreenSize().getHeight();           //900
     BufferedImage image = ImageIO.read(new URL("https://gamerwall.pro/uploads/posts/2021-11/1637571551_12-gamerwall-pro-p-abstraktnie-treugolniki-krasivie-oboi-12.png"));
+    BufferedImage image1 = ImageIO.read(new File("src/img_2.png"));
     BufferedImage person = ImageIO.read(new URL("https://phonoteka.org/uploads/posts/2021-07/1625663703_35-phonoteka-org-p-soul-knait-razboinik-art-krasivo-36.png"));
     public Window() throws IOException {
         super();
@@ -33,7 +43,7 @@ public class Window extends JFrame implements ActionListener, KeyListener {
         this.setResizable(false);
         panel = new JPanel();
         //this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        this.getContentPane().add(panel);
+        getContentPane().add(panel);
         this.pack();
         this.setSize(xssize, yssize);
         //this.setAlwaysOnTop(true);
@@ -44,16 +54,14 @@ public class Window extends JFrame implements ActionListener, KeyListener {
         setVisible(true);
     }
 
-    public void paint(Graphics g){
-        super.paint(g);
-        g.drawImage(image, 0, 0, null);
-        g.drawImage(person, 0, 0, WIDTH, HEIGHT, null);
-        t.start();
+    protected void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        g.drawImage(image1, 0, 0, this); // see javadoc for more info on the parameters
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        repaint();
+       repaint();
     }
 
     @Override
@@ -64,7 +72,7 @@ public class Window extends JFrame implements ActionListener, KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode()==KeyEvent.VK_RIGHT){
-            person.setLocation(x+1, y);
+            //person.setLocation(x+1, y);
             x += 1;
         }
     }
